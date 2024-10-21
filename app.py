70b9fd1d140936f296643a75cb9688820ca2ddcc
Noah Karlethsofer <Noah.karletshofer@uni-ulm.de>
2024-10-21 14:39:59 +0200

    added functionality: own querries
    fixed a problem where without the gpt modell lida.visualize wouldnt gibe a response

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import lida
-
 import streamlit as st 
 from lida import Manager, TextGenerationConfig , llm  
 from dotenv import load_dotenv
@@ -55,6 +53,7 @@ if 'button_states' not in st.session_state:
         'show_below_value': False,
         'show_between_values': False,
         'show_one_category': False,
+        'change_chart_type' : False,
         'change_chart_type_better_fit': False
     }
 # Function to handle button clicks
@@ -100,6 +99,8 @@ def edit(charts, data, type, input_value=None):
         input = handle_input("show between values (between which values? divide with 'and')", test2.show_between_values, input_value)
     elif type == "show_one_category":
         input = handle_input("show one category (which category?)", test2.show_one_category, input_value)
+    elif type == "change_chart_type":
+        input = handle_input("change chart type", test2.change_chart_type, input_value)
     elif type == "change_chart_type_better_fit":
         input = handle_input("change chart type better fit (which chart type?)", test2.change_chart_type_better_fit, input_value)
 
@@ -131,26 +132,30 @@ def edit(charts, data, type, input_value=None):
 
 def createDiagramm():
     library = "altair"
-    textgen_config = TextGenerationConfig(n=1, temperature=0.2, use_cache=True)
+    textgen_config = TextGenerationConfig(n=1, temperature=0.2, model="gpt-4-turbo", use_cache=True)
     summary = st.session_state.summary
-    if st.session_state.goalNumber == 1000:
-        #charts = lida.visualize(summary, goal=custom_goal, textgen_config=textgen_config, library=library)  
-        #tempGoal = "what is the correleation between total rooms and population?"
-        #ownGoal = st.session_state.ownGoal
-        st.write("debug here")
-        st.write("summary:",summary)
-        st.write("textgen_config:",textgen_config)
-        st.write("library:",library)
-        charts = lida.visualize(summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
-        #st.write(custom_goal.type)
-        st.write(charts)
-    else:
-        st.write("debug here")
-        st.write("summary:",summary)
-        st.write("textgen_config:",textgen_config)
-        st.write("library:",library)
-        #charts = lida.visualize(summary, goal=st.session_state.goals[st.session_state.goalNumber], textgen_config=textgen_config, library=library)  
-        charts = lida.visualize(summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
+    #st.write(st.session_state.goals)
+    charts = lida.visualize(summary, goal=st.session_state.goals[st.session_state.goalNumber], textgen_config=textgen_config, library=library)  
+    # if st.session_state.goalNumber == 1000:
+    #     #charts = lida.visualize(summary, goal=custom_goal, textgen_config=textgen_config, library=library)  
+    #     #tempGoal = "what is the correleation between total rooms and population?"
+    #     #ownGoal = st.session_state.ownGoal
+    #     st.write("debug here goalnumber == 1000")
+    #     st.write("summary:",summary)
+    #     st.write("textgen_config:",textgen_config)
+    #     st.write("library:",library)
+    #     charts = lida.visualize(summary, goal=st.session_state.ownGoal, textgen_config=textgen_config, library=library)  
+    #     #st.write(custom_goal.type)
+    #     st.write(charts)
+    # else:
+    #     st.write("debug here goalnumber == ", st.session_state.goalNumber)
+    #     st.write("summary:",summary)
+    #     st.write("textgen_config:",textgen_config)
+    #     st.write("library:",library)
+    #     st.write("goals: ", st.session_state.goals)
+    #     charts = lida.visualize(summary, goal=st.session_state.goals[st.session_state.goalNumber], textgen_config=textgen_config, library=library)  
+    #     #charts = lida.visualize(summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
+    #     st.write(charts)
     
     #altair error fix 
     #this way would be way better for perfomance ut I didnt get it to run
@@ -204,85 +209,87 @@ def display_second_page():
 def display_first_page():
     textgen_config = TextGenerationConfig(n=1, temperature=0.5, model="gpt-4-turbo", use_cache=True)
 
-    menu = st.sidebar.selectbox("Choose an Option", ["Summarize", "Question based Graph"])
+    #menu = st.sidebar.selectbox("Choose an Option", ["Summarize", "Question based Graph"])
 
-    if menu == "Summarize":
-        st.subheader("Summarization of your Data")
-        file_uploader = st.file_uploader("Upload your CSV", type="csv")
-        if file_uploader is not None:
-            #path_to_save = "filename.csv"
-            path_to_save = os.path.abspath("filename.csv")
-            with open(path_to_save, "wb") as f:
-                f.write(file_uploader.getvalue())
+    #if menu == "Summarize":
+    st.subheader("Summarization of your Data")
+    file_uploader = st.file_uploader("Upload your CSV", type="csv")
+    if file_uploader is not None:
+        #path_to_save = "filename.csv"
+        path_to_save = os.path.abspath("filename.csv")
+        with open(path_to_save, "wb") as f:
+            f.write(file_uploader.getvalue())
 
-            data = pd.read_csv(path_to_save)
-            st.write(data.head())
+        data = pd.read_csv(path_to_save)
+        st.write(data.head())
 
-            summary = lida.summarize(path_to_save, summary_method="default", textgen_config=textgen_config)
-            goals = lida.goals(summary, n=2, textgen_config=textgen_config)
+        summary = lida.summarize(path_to_save, summary_method="default", textgen_config=textgen_config)
+        goals = lida.goals(summary, n=2, textgen_config=textgen_config)
 
-            #this doesnt exist "st.write(summary.file_name)" why is it working then?
-            st.session_state.summary = summary
-            st.session_state.goals = goals
-            st.session_state.data = data
+        #this doesnt exist "st.write(summary.file_name)" why is it working then?
+        st.session_state.summary = summary
+        st.session_state.goals = goals
+        st.session_state.data = data
 
-            goalNumber=0
-            for goal in goals:
-                st.write(goal.question)
-                toggle = st.checkbox("choose this goal",value=False, key=goalNumber)
-                with st.expander("see rational and visualization"):
-                    st.write(goal.rationale + "\n\n" + goal.visualization)
-                if toggle:
-                    st.session_state.goalNumber = goalNumber
-                    st.session_state.page = "second"
-                    #createDiagramm(i, summary, goals, data)
-                goalNumber=goalNumber+1
+        goalNumber=0
+        for goal in goals:
+            st.write(goal.question)
+            toggle = st.checkbox("choose this goal",value=False, key=goalNumber)
+            with st.expander("see rational and visualization"):
+                st.write(goal.rationale + "\n\n" + goal.visualization)
+            if toggle:
+                st.session_state.goalNumber = goalNumber
+                st.session_state.page = "second"
+                #createDiagramm(i, summary, goals, data)
+            goalNumber=goalNumber+1
             
             #goals.append("Write your own goal")
-            #####               what is the correleation between total rooms and population?
-            ownGoal = st.text_input("Enter your own goal for Lida")
-            if st.button("Submit"):
-                if len(ownGoal) > 0:
-                    st.session_state.goalNumber = 1000
-                    st.session_state.ownGoal = ownGoal
-                    st.session_state.page = "second"
+            #####               what is the correleation between total rooms and population?   
+        ownGoal = st.text_input("Enter your own goal for Lida")
+        if st.button("Submit"):
+            if len(ownGoal) > 0:
+                goals.append(ownGoal)
+                st.session_state.goalNumber = len(goals) -1
+                st.session_state.goals = goals
+                #st.session_state.ownGoal = ownGoal
+                st.session_state.page = "second"
 
-                    # st.write(ownGoal)
-                    # library = "altair"
-                    # st.write("debug here")
-                    # st.write("summary:",summary)
-                    # st.write("textgen_config:",textgen_config)
-                    # st.write("library:",library)
-                    # #charts = lida.visualize(summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
-                    # charts = lida.visualize(summary, goal=st.session_state.goals[0], textgen_config=textgen_config, library=library)  
-                    # st.write(charts)
+                # # st.write(ownGoal)
+                # library = "altair"
+                # # #st.write("debug here")
+                # st.write("summary:",summary)
+                # st.write("textgen_config:",textgen_config)
+                # st.write("library:",library)
+                # charts = lida.visualize(summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
+                # # #charts = lida.visualize(summary, goal=st.session_state.goals[0], textgen_config=textgen_config, library=library)  
+                # st.write(charts)
 
                 #createDiagramm(len(goals), summary, goals, data)
         
 ############### THIS WAS A PAGE WHERE YOU COULD WRITE YOUR OWN GOAL TO LIDA ####################
 # SOMETHING LIKE THAT WILL BE NEEDED FOR THE FINAL PRODUCT
 
-    elif menu == "Question based Graph":
-        st.subheader("Query your Data to Generate Graph")
-        file_uploader = st.file_uploader("Upload your CSV", type="csv")
-        if file_uploader is not None:
-            path_to_save = "filename1.csv"
-            with open(path_to_save, "wb") as f:
-                f.write(file_uploader.getvalue())
-            text_area = st.text_area("Query your Data to Generate Graph", height=200)
-            if st.button("Generate Graph"):
-                if len(text_area) > 0:
-                    st.info("Your Query: " + text_area)
-                    lida = Manager(text_gen = llm("openai")) 
-                    textgen_config = TextGenerationConfig(n=1, temperature=0.2, use_cache=True)
-                    summary = lida.summarize("filename1.csv", summary_method="default", textgen_config=textgen_config)
-                    user_query = text_area
-                    library = "altair"
-                    charts = lida.visualize(summary=summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
-                    charts[0]
-                    image_base64 = charts[0].raster
-                    img = base64_to_image(image_base64)
-                    st.image(img)
+    # elif menu == "Question based Graph":
+    #     st.subheader("Query your Data to Generate Graph")
+    #     file_uploader = st.file_uploader("Upload your CSV", type="csv")
+    #     if file_uploader is not None:
+    #         path_to_save = "filename1.csv"
+    #         with open(path_to_save, "wb") as f:
+    #             f.write(file_uploader.getvalue())
+    #         text_area = st.text_area("Query your Data to Generate Graph", height=200)
+    #         if st.button("Generate Graph"):
+    #             if len(text_area) > 0:
+    #                 st.info("Your Query: " + text_area)
+    #                 lida = Manager(text_gen = llm("openai")) 
+    #                 textgen_config = TextGenerationConfig(n=1, temperature=0.2, use_cache=True)
+    #                 summary = lida.summarize("filename1.csv", summary_method="default", textgen_config=textgen_config)
+    #                 user_query = text_area
+    #                 library = "altair"
+    #                 charts = lida.visualize(summary=summary, goal="what is the correleation between total rooms and population?", textgen_config=textgen_config, library=library)  
+    #                 charts[0]
+    #                 image_base64 = charts[0].raster
+    #                 img = base64_to_image(image_base64)
+    #                 st.image(img)
 
 
 ############ THIS WAS USED PREVIOUSLY IF WE USE A METHOD LIKE IN THE COMMENTED CODE AT THE BOTTOM WE CAN USE THIS ##############
